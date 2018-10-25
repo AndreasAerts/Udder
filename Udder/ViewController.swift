7078390337ca58f163e7dd540ca0bd465ce593d7
AndreasAerts <r0658500@student.thomasmore.be>
2018-10-25 17:26:51 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Andreas Aerts on 25/10/2018.
+//  Copyright © 2018 Andreas Aerts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
